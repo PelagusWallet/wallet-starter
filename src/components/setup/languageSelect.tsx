04de682b93ff7c1ab6f8b9cf1df86273a5f30c2f
Alan Orwick <alan.kev11@gmail.com>
2023-05-19 14:40:01 -0500

    Cleanup language select

--- src/components/setup/languageSelect.tsx
+++ src/components/setup/languageSelect.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "wouter"
+import React, { useEffect, useRef, useState } from "react"
 
 const languages = [
   { id: "english", name: "English" },
@@ -12,28 +12,60 @@ const languages = [
 ]
 
 export default function LanguageSelect() {
-  const [, setLocation] = useLocation()
+  const [dropdownOpen, setDropdownOpen] = useState(false)
+  const [selectedLanguage, setSelectedLanguage] = useState("English")
+  const dropdownRef = useRef(null)
 
-  const handleSelect = (id) => {
-    // here you would probably update some global state or local storage with the selected language id
-    // then navigate to the next page, or back to the previous one
+  const handleSelect = (id, name) => {
     console.log("selected language id:", id)
-    // setLocation("/setup")
+    setSelectedLanguage(name)
+    setDropdownOpen(false)
   }
 
+  useEffect(() => {
+    const clickListener = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false)
+      }
+    }
+
+    document.addEventListener("click", clickListener)
+    return () => {
+      document.removeEventListener("click", clickListener)
+    }
+  }, [])
+
   return (
-    <div className="fixed top-10 right-10 text-white">
-      <div className="w-full">
-        <select
-          onChange={(event) => handleSelect(event.target.value)}
-          className="text-white w-full p-3 mx-2 text-center text-lg font-quai bg-transparent border rounded-md border-gray-200 cursor-pointer">
-          {languages.map((language) => (
-            <option className="mx-2" key={language.id} value={language.id}>
-              {language.name}
-            </option>
-          ))}
-        </select>
+    <div className="fixed top-10 right-20 text-white" ref={dropdownRef}>
+      <div
+        className="flex flex-row relative text-white w-full p-3 mx-2 text-center text-lg font-quai bg-transparent border rounded-md border-gray-200 cursor-pointer"
+        onClick={() => setDropdownOpen(!dropdownOpen)}>
+        {selectedLanguage}
+        <div className="m-auto">
+          <svg
+            className="h-6 w-6 text-gray-500"
+            fill="currentColor"
+            viewBox="0 0 20 20">
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </div>
       </div>
+      {dropdownOpen && (
+        <div className="absolute right-0 w-full mt-2 py-2 bg-white rounded-lg shadow-xl">
+          {languages.map((language) => (
+            <div
+              onClick={() => handleSelect(language.id, language.name)}
+              className="cursor-pointer block px-4 py-2 text-gray-800 hover:bg-zinc-950 hover:text-white"
+              key={language.id}>
+              {language.name}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }

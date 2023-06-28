098af545c1411458eb3a91ebb835198eeb3746aa
Alan Orwick <alan.kev11@gmail.com>
2023-06-28 09:59:32 -0500

    Cleanup the MenuPopup

--- src/components/navigation/MenuPopup.tsx
+++ src/components/navigation/MenuPopup.tsx
@@ -6,13 +6,24 @@ import { getAddresses, setActiveAddress } from "~storage/wallet"
 
 import "../../style.css"
 
+import { CheckCircleIcon } from "@heroicons/react/24/outline"
 import { Cog6ToothIcon, PlusCircleIcon } from "@heroicons/react/24/solid"
 
+import { Storage } from "@plasmohq/storage"
+import { useStorage } from "@plasmohq/storage/hook"
+
 import { formatAddress } from "~utils/format"
+
+const storage = new Storage({ area: "local" })
 
 export default function MenuPopup({ onClicked }) {
   const [location, setLocation] = useLocation()
   const [addresses, setAddresses] = useState<Address[]>([])
+
+  const [activeAddress] = useStorage<Address>({
+    key: "active_address",
+    instance: storage
+  })
 
   useEffect(() => {
     async function fetchAddresses() {
@@ -41,27 +52,35 @@ export default function MenuPopup({ onClicked }) {
     <div>
       <div className="absolute w-screen h-screen" onClick={onClicked}></div>
       <div className="absolute top-[70px] p-2 w-full">
-        <div className="modal-content secondary-bg-container cursor-pointer border border-3 ">
+        <div className="modal-content secondary-bg-container cursor-pointer border border-3 p-0">
+          <div className="border-b">
+            <div className="m-2 text-base">My accounts</div>
+          </div>
           {addresses.map((address) => {
             return (
               <div
                 onClick={() => clickAddress(address)}
-                className="flex flex-col mb-3 ml-3">
-                <div>{address.name}</div>
+                className="flex flex-row mb-3 m-2">
+                {address.address === activeAddress.address ? (
+                  <CheckCircleIcon className="h-6 w-6 my-auto mr-2 text-green-600"></CheckCircleIcon>
+                ) : (
+                  <div className="h-6 w-6 my-auto mr-2"></div>
+                )}
+                <div className="items-center my-auto">{address.name}</div>
               </div>
             )
           })}
-          <div className="flex flex-col space-y-4 mt-2">
-            <div className="border-b-1" onClick={clickAddAddress}>
-              <div className="flex flex-row">
+          <div className="flex flex-col mt-2">
+            <div className="border-t-2" onClick={clickAddAddress}>
+              <div className="flex flex-row m-2">
                 <PlusCircleIcon className="h-6 w-6 my-auto" />
                 <div className="block rounded-md px-3 text-base font-medium">
                   Add Account
                 </div>
               </div>
             </div>
-            <div className="border-b-1" onClick={clickSettingsButton}>
-              <div className="flex flex-row">
+            <div onClick={clickSettingsButton}>
+              <div className="flex flex-row m-2">
                 <Cog6ToothIcon className="h-6 w-6 my-auto" />
                 <div className="block rounded-md px-3 text-base font-medium">
                   Settings
